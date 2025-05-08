7121005929c9e3ec9d05bc1d35b1b3da7d2c7c3a
daniil-shlenskii <daniil.shlenskii@gmail.com>
2025-05-08 18:17:30 +0000

    potentials update

--- src/ott2/neural/methods/lagrangian/lagrangian_potentials.py
+++ src/ott2/neural/methods/lagrangian/lagrangian_potentials.py
@@ -1,13 +1,16 @@
+import functools
 from abc import abstractmethod
-import jax.numpy as jnp
+from dataclasses import dataclass
+
 import jax
-from jax import nn
+import jax.numpy as jnp
 import numpy as np
+from jax import nn
+
+from flax.struct import PyTreeNode
+
 from ott2.datasets import create_lagrangian_ds
 
-import functools
-from dataclasses import dataclass
-from flax.struct import PyTreeNode
 
 class LagrangianPotentialBase(PyTreeNode):
     D: int = 2
@@ -87,6 +90,7 @@ class SlitPotential(LagrangianPotentialBase):
     ymin: float = -0.25
     ymax: float = 0.25
     M_bounds = (0., 1.)
+    temp_bounds = (1e-1, 2e-3)
 
     x_axes_bounds = (-1.5, 1.5)
     y_axes_bounds = (-2., 2.)
@@ -109,12 +113,12 @@ class SlitPotential(LagrangianPotentialBase):
         return U
 
 class BabyMazePotential(LagrangianPotentialBase):
-    xmin1: float = -0.5
+    xmin1: float = -0.6
     xmax1: float = -0.3
     ymin1: float = -1.99
     ymax1: float = -0.15
     xmin2: float = 0.3
-    xmax2: float = 0.5
+    xmax2: float = 0.6
     ymin2: float = 0.15
     ymax2: float = 1.99
     M_bounds = (0., 10.)
@@ -167,13 +171,26 @@ class GSB_GMM_Potential(LagrangianPotentialBase):
     M_bounds = (0., 0.1)
     temp_bounds = (1., 0.1)
 
+    x_axes_bounds = (-19., 19.)
+    y_axes_bounds = (-19., 19.)
+
+    sampler_func = functools.partial(create_lagrangian_ds, geometry_str='gmm')
+
+    def get_samples(self, size, key):
+        vneck_sampler = self.sampler_func(batch_size=size, key=key)
+        sampler = next(iter(vneck_sampler))
+        source_data = sampler['src_lin']
+        target_data = sampler['tgt_lin']
+        return source_data, target_data
+
     def __call__(self, x):
         assert x.ndim == 1 and x.shape[0] == self.D
 
         V = 0.
         for i in range(self.centers.shape[0]):
             dist = jnp.linalg.norm(x - self.centers[i])
-            V -= self.M * nn.sigmoid((self.radius - dist) / self.temp)
+            V -= nn.softplus(100 * (self.radius - dist)) * (self.radius > dist).astype(jnp.int32)
+
         return V
 
 class VNeck_Potential(LagrangianPotentialBase):
@@ -201,6 +218,88 @@ class VNeck_Potential(LagrangianPotentialBase):
         d = self.coef * xs_sq[0] - xs_sq[1]
 
         return - self.M * nn.sigmoid((-self.c_sq - d) / self.temp)
+    
+
+class VNeck_bench(LagrangianPotentialBase):
+
+    x_axes_bounds = (-8., 8.)
+    y_axes_bounds = (-2., 2.)
+
+    sampler_func = functools.partial(create_lagrangian_ds, geometry_str='vneck')
+    
+    def get_samples(self, size, key):
+        box_sampler = self.sampler_func(batch_size=size, key=key)
+        sampler = next(iter(box_sampler))
+        source_data = sampler['src_lin']
+        target_data = sampler['tgt_lin']
+        return source_data, target_data
+
+
+    def obstacle_cfg_vneck(self):
+        c_sq = 0.36
+        coef = 5
+        return c_sq, coef
+
+
+    def __call__(self, xt):
+    
+        assert xt.shape[-1] == 2
+
+        c_sq, coef = self.obstacle_cfg_vneck()
+
+        xt_sq = xt * xt
+        d = coef * xt_sq[0] - xt_sq[1]
+
+        cond = jnp.astype(jax.nn.softplus((-c_sq - d)) > 0.53, jnp.int32)
+        cond_1 = jnp.astype(jax.nn.softplus((-c_sq - d)) < 5, jnp.int32)
+
+        return -jax.nn.softplus((-c_sq - d)) * cond * cond_1 - 5 * (1 - cond_1)
+    
+
+class STunnel_bench(LagrangianPotentialBase):
+
+    x_axes_bounds = (-13, 13)
+    y_axes_bounds = (-13, 13)
+
+
+    sampler_func = functools.partial(create_lagrangian_ds, geometry_str='stunnel')
+    
+    def get_samples(self, size, key):
+        box_sampler = self.sampler_func(batch_size=size, key=key)
+        sampler = next(iter(box_sampler))
+        source_data = sampler['src_lin']
+        target_data = sampler['tgt_lin']
+        return source_data, target_data
+
+
+    def obstacle_cfg(self):
+        a, b, c = 20, 1, 90
+        centers = [[5, 6], [-5, -6]]
+        return a, b, c, centers
+
+
+    def __call__(self, xt):
+
+
+        a, b, c, centers = self.obstacle_cfg()
+
+        D = xt.shape[0]
+        assert D == 2
+
+        _xt = xt.reshape(D)
+        x, y = _xt[0], _xt[1]
+
+        d = a * (x - centers[0][0]) ** 2 + b * (y - centers[0][1]) ** 2
+        # c1 = 1500 * (d < c)
+        c1 = nn.softplus(c - d)
+
+        d = a * (x - centers[1][0]) ** 2 + b * (y - centers[1][1]) ** 2
+        # c2 = 1500 * (d < c)
+        c2 = nn.softplus(c - d)
+
+        cost = (c1 + c2)
+        return -cost
+
 
 
 class STunnel_Potential(LagrangianPotentialBase):
